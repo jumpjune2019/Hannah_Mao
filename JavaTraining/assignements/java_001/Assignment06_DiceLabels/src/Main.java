787b32946a917d3aa39160ae0da6bb9f1194d157
Hannah Mao <hannahmao@Hannahs-MacBook-Pro.local>
2019-07-08 14:53:59 -0500

    push with doc

--- JavaTraining/assignements/java_001/Assignment06_DiceLabels/src/Main.java
+++ JavaTraining/assignements/java_001/Assignment06_DiceLabels/src/Main.java
@@ -11,23 +11,22 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 
-		
-		//initialize the matrix
-		String[][] matrix = {
-				{" ","Die1		","Die2   	","Die3	   	","Die4	 ","Die5		","Die6	"},
-				{"Die1","Snake eyes","        Australian yo","Little Joe From Kokomo","No field five","Easy six","        Six one you're done"},
-				{"Die2","Ace caught a deuce","Ballerina","The fever","        Jimmie Hicks","Benny Blue","        Easy eight"},
-				{"Die3","Easy four","        OJ","        Brooklyn Forest","        Big Red","        Eighter from Decatur","Nina from Pasadena"},
-				{"Die4","Little Pheobe","        Easy six","Skinny Mckinney","        Square pair","Railroad nine","        Big one on the end"},
-				{"Die5","Sixie from Dixie","Skinny Dugan","Easy eight","        Jesse James","Puppy paws","        Yo"},
-				{"Die6","Devil","                Easy eight","Lou Brown","        Tennessee","Six five no jive","Midnight"},					
+	String[][] matrix = {
+				{" ","Die1","Die2","Die3","Die4","Die5","Die6"},
+				{"Die1","Snake eyes","Australian yo","Little Joe From Kokomo","No field five","Easy six","Six one you're done"},
+				{"Die2","Ace caught a deuce","Ballerina","The fever","Jimmie Hicks","Benny Blue","Easy eight"},
+				{"Die3","Easy four","OJ","Brooklyn Forest","ig Red","Eighter from Decatur","Nina from Pasadena"},
+				{"Die4","Little Pheobe","Easy six","Skinny Mckinney","Square pair","Railroad nine","Big one on the end"},
+				{"Die5","Sixie from Dixie","Skinny Dugan","Easy eight","Jesse James","Puppy paws","Yo"},
+				{"Die6","Devil","Easy eight","Lou Brown","Tennessee","Six five no jive","Midnight"},					
 		};
 		
-	//print the matrix
-	System.out.println("Dice Combinations Label Matrix");
-  
-       for(String[] row : matrix) {
-          printRow(row);
+		for (int i = 0; i < matrix.length; i++) {
+		    for (int j = 0; j < matrix[i].length; j++) {
+		        System.out.printf("%-25s", matrix[i][j]);
+		        
+		    }
+		   System.out.println();
        }    
 	}
 }
