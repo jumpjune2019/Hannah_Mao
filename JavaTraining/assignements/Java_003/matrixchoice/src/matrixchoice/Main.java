977e806e66b83d3cd0373cfc6ab9668b9acee09e
Hannah Mao <hannahmao@Hannahs-MacBook-Pro.local>
2019-07-09 08:16:03 -0500

    push with doc

--- JavaTraining/assignements/Java_003/matrixchoice/src/matrixchoice/Main.java
+++ JavaTraining/assignements/Java_003/matrixchoice/src/matrixchoice/Main.java
@@ -1,8 +1,17 @@
 package matrixchoice;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		File file = new File ("/Users/hannahmao/Desktop/JavaTraining/assignements/Java_003/matrixchoice/file1.txt");
+		PrintWriter printWriter = new PrintWriter (file);
+		
 		
 		String[][] matrix = {
 				{" ","Die1","Die2","Die3","Die4","Die5","Die6"},
@@ -14,14 +23,66 @@ public class Main {
 				{"Die6","Devil","Easy eight","Lou Brown","Tennessee","Six five no jive","Midnight"},					
 		};
 		
-		for (int i = 0; i < matrix.length; i++) {
-		    for (int j = 0; j < matrix[i].length; j++) {
-		        System.out.printf("%-25s", matrix[i][j]);
-		        
-		    }
-		   System.out.println();
-		}
 		
+		System.out.println("What do you want?");
+		System.out.println("1) Print on screen only (press 1)");
+		System.out.println("2) Print on file (press2)");
+		System.out.println("3) Print on both (press3)");
+		
+		Scanner s= new Scanner(System.in);
+		
+		
+			//press 1 to print on the screen
+			if(s.hasNext("1")) {
+		
+				for (int i = 0; i < matrix.length; i++) {
+					for (int j = 0; j < matrix[i].length; j++) {
+					System.out.printf("%-24s", matrix[i][j]);
+		        
+				    }
+					System.out.println();
+			     }
+				//break;
+			}	
+		
+			//press 2 to print on file
+			if(s.hasNext("2")) {
+				
+		
+				for (int i = 0; i < matrix.length; i++) {
+					for (int j = 0; j < matrix[i].length; j++) {
+						printWriter.printf("%-23s", matrix[i][j]);
+        
+					}
+					printWriter.println();
+				}
+	
+				printWriter.close ();  
+				
+			}
+			
+			//press 3 to print to the screen and print on file
+			if(s.hasNext("3")) {
+				
+				
+				for (int i = 0; i < matrix.length; i++) {
+					for (int j = 0; j < matrix[i].length; j++) {
+					System.out.printf("%-24s", matrix[i][j]);
+				    }
+					System.out.println();
+			     }
+				
+				
+				for (int i = 0; i < matrix.length; i++) {
+					for (int j = 0; j < matrix[i].length; j++) {
+						printWriter.printf("%-23s", matrix[i][j]);
+        
+					}
+					printWriter.println();
+				   }
+				printWriter.close ();  
+				
+			  }  		
+			
 	}
-
 }
