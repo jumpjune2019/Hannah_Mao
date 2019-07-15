8401cdae91922e101d006b182431d6ebd8fbee2d
Hannah Mao <hannahmao@Hannahs-MacBook-Pro.local>
2019-07-15 16:14:25 -0500

    push w/ doc

---
+++ JavaTraining/assignments/Java_006/prelude_to_lambda/Arrow_Operator_101/arrowDemo.js
@@ -0,0 +1,12 @@
+
+var doc = () =>{console.log("Hello World");}
+doc();
+
+
+
+var setNameIdsEs6 = (id, name) => ({ id: id, name: name });
+
+console.log(setNameIdsEs6 (4, "Hannah"));   // Object {id: 4, name: "Kyle"}
+
+
+
